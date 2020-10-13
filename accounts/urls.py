f76dd8488da0ab9767f3ddf2851a3290165b089b
Benito Buchheim <github.benito.2017@buchheims.de>
2020-10-13 14:27:15 +0200

    created base template and view for user profiles

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from . import views
 
 app_name = 'accounts'
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('profile/', views.profile, name='profile'),
 ]
