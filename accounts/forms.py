43ebd4dceed9c9780f694c6d594c71321456c6fd
Benito Buchheim <github.benito.2017@buchheims.de>
2020-10-13 20:22:57 +0200

    fixed linting errors

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,12 +7,17 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class RegistrationForm(UserCreationForm):
+    #pylint: disable=too-few-public-methods
     """The form used for user registration on the site."""
     first_name  = forms.CharField(max_length=30, required=False, help_text="Optional.")
     last_name   = forms.CharField(max_length=30, required=False, help_text="Optional.")
-    email       = forms.EmailField(max_length=254, help_text='Required. Enter a valid email address.')
+    email       = forms.EmailField(
+        max_length=254,
+        help_text='Required. Enter a valid email address.'
+    )
 
     class Meta:
+        """RegistrationForms metadata."""
         model = User
         fields = (
             'username',
--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@
 Contains the Views for the Accounts app."
 """
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 
