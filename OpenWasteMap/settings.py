567262ab087d12a91a633fa550707beb60866d63
Benito Buchheim <github.benito.2017@buchheims.de>
2020-10-14 11:38:48 +0200

    Created the map_viewer app

--- OpenWasteMap/settings.py
+++ OpenWasteMap/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
+    'map_viewer.apps.MapViewerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
