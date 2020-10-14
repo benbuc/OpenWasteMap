18359c6975b80f00a9bf984e75d96788d246576b
Benito Buchheim <github.benito.2017@buchheims.de>
2020-10-14 15:19:33 +0200

    added first css functionality using django_sass

--- OpenWasteMap/settings.py
+++ OpenWasteMap/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_sass',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
+)
