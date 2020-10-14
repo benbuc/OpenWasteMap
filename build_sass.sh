6b1fa1e5c3cc352af3d5e5a4c98c8c3cef115702
Benito Buchheim <github.benito.2017@buchheims.de>
2020-10-14 16:02:48 +0200

    added basic sass configuration and mixins

--- build_sass.sh
+++ build_sass.sh
@@ -1,4 +1,4 @@
 # run to compile sass files
 # run with --watch to watch for changes and automatically compile again
 
-python3 manage.py sass static/scss/styles.scss static/css/styles.css $1
+python3 manage.py sass static/scss/styles.scss static/css/styles.css -t compressed $1
