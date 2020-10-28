bed025047b02a27cad7ae1cf54b002269a24a654
Benito <34064622+benbuc@users.noreply.github.com>
2020-10-28 11:04:32 +0100

    Email addresses must be unique (#41)
    * email address must be unique
    error message is shown
    
    * test that duplicate email shows error message

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,9 +5,9 @@ Contains different subclasses of formulars.
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 
 class RegistrationForm(UserCreationForm):
-    #pylint: disable=too-few-public-methods
     """The form used for user registration on the site."""
     first_name  = forms.CharField(max_length=30, required=False, help_text="Optional.")
     last_name   = forms.CharField(max_length=30, required=False, help_text="Optional.")
@@ -15,6 +15,19 @@ class RegistrationForm(UserCreationForm):
         max_length=254,
         help_text='Required. Enter a valid email address.'
     )
+
+    def clean_email(self):
+        """Clean email to be unique"""
+
+        email = self.cleaned_data.get('email')
+
+        if get_user_model().objects.filter(email=email).exists():
+            raise ValidationError(
+                "Email already exists",
+                code="email_exists"
+            )
+
+        return email
 
     class Meta:
         """RegistrationForms metadata."""
