536859b3e498d5ac1127b5e288b8654356f17fd2
Benito Buchheim <github.benito.2017@buchheims.de>
2021-11-01 13:27:00 +0100

    Bumped version number to 0.4

--- OpenWasteMap/settings.py
+++ OpenWasteMap/settings.py
@@ -146,4 +146,4 @@ EMAIL_MAIL_PLAIN = "registration/verify_email.txt"
 EMAIL_PAGE_TEMPLATE = "registration/process_email_verification.html"
 EMAIL_PAGE_DOMAIN = "http://www.openwastemap.org/"
 
-OWM_VERSION = "0.3"
+OWM_VERSION = "0.4"
