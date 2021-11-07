33eaf27b3c70b1e5e5d8f381198ea5b58ffe1d9d
Benito Buchheim <github.benito.2017@buchheims.de>
2021-11-07 15:22:47 +0100

    refactoring

--- openwastemap/tile_server/tasks.py
+++ openwastemap/tile_server/tasks.py
@@ -13,10 +13,10 @@ from .render import TileRenderer
 @task(name="render_tile")
 def render_tile(zoom, xcoord, ycoord):
     rendered_tile = TileRenderer(zoom, xcoord, ycoord).render()
-    out_path = (
-        Path(settings.TILES_ROOT) / str(zoom) / str(xcoord) / (str(ycoord) + ".png")
-    )
     if not settings.DEBUG or settings.CHECK_TILE_CACHE_HIT:
+        out_path = (
+            Path(settings.TILES_ROOT) / str(zoom) / str(xcoord) / (str(ycoord) + ".png")
+        )
         out_path.parent.mkdir(parents=True, exist_ok=True)
         rendered_tile.save(out_path)
     return rendered_tile
