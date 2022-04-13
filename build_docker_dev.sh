5dd467485b335be78e312e3cc94c055576af9228
Benito Buchheim <github.benito.2017@buchheims.de>
2022-04-13 16:52:04 +0200

    started new project using VueJS and FastAPI

--- build_docker_dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p ./build
-poetry export --dev -f requirements.txt --output ./build/requirements.txt --without-hashes
-
-docker-compose --file docker-compose.dev.yml build --pull
-docker-compose --file docker-compose.dev.yml pull
--- openwastemap/static/js/main_menu.js
+++
@@ -1,9 +0,0 @@
-function toggleMobileMenu() {
-    var mobileNav = document.getElementById("mobile-nav")
-    mobileNav.classList.toggle('active');
-}
-
-document.addEventListener("DOMContentLoaded", function(){
-    var mobileMenuButton = document.getElementById("mobile-nav-button");
-    mobileMenuButton.addEventListener("click", toggleMobileMenu);
-});
