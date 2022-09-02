b3ddba748dea2513c319fdc12ef177ba6d78206d
Benito Buchheim <github.benito.2017@buchheims.de>
2022-09-02 11:35:16 +0200

    added very basic POC profile view

---
+++ frontend/src/views/ProfileIndex.vue
@@ -0,0 +1,11 @@
+<template>
+    <p>Hello this is your profile</p>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ProfileIndex extends Vue {
+}
+</script>
