ff780cb7a29e0d1cb5e47c2d1b15b59c8abb8622
Benito Buchheim <github.benito.2017@buchheims.de>
2022-09-04 20:29:39 +0200

    Map starting zoomed to Berlin

--- frontend/src/components/OwmMap.vue
+++ frontend/src/components/OwmMap.vue
@@ -1,5 +1,5 @@
 <template>
-  <l-map style="z-index: 0">
+  <l-map style="z-index: 0" :zoom="zoom" :center="center">
     <l-tile-layer :url="urlOSM" :attribution="attributionOSM"></l-tile-layer>
     <l-tile-layer :url="urlOWM" :attribution="attributionOWM"></l-tile-layer>
     <l-marker v-if="currentPosition" :lat-lng="currentPosition"></l-marker>
@@ -9,9 +9,12 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import { api } from "@/api";
+import L from "leaflet";
 
 @Component
 export default class Map extends Vue {
+  public zoom = 11;
+  public center = [52.5183, 13.4006];
   public urlOSM = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
   public get urlOWM() {
     return api.getTilesEndpoint();
