f54ee889d5dad0b45374184c8b238bed524274a2
Benito Buchheim <github.benito.2017@buchheims.de>
2022-09-07 19:36:41 +0200

    add test for password reset

--- backend/app/app/tests/api/api_v1/test_login.py
+++ backend/app/app/tests/api/api_v1/test_login.py
@@ -1,8 +1,13 @@
 from typing import Dict
 
 from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
 
 from app.core.config import settings
+from app.core.security import verify_password
+from app.tests.utils.user import create_random_user
+from app.utils import generate_password_reset_token
+from app.tests.utils.utils import random_lower_string
 
 
 def test_get_access_token(client: TestClient) -> None:
@@ -26,3 +31,16 @@ def test_use_access_token(
     result = r.json()
     assert r.status_code == 200
     assert "email" in result
+
+
+def test_reset_password_correct_token(client: TestClient, db: Session) -> None:
+    user = create_random_user(db)
+    password_reset_token = generate_password_reset_token(email=user.email)
+    new_password = random_lower_string()
+    r = client.post(
+        f"{settings.API_V1_STR}/reset-password",
+        json={"token": password_reset_token, "new_password": new_password},
+    )
+    assert r.status_code == 200
+    db.refresh(user)
+    assert verify_password(new_password, user.hashed_password)
