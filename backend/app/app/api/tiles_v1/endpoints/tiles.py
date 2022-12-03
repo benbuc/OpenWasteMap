8272a13a2c3661e6124504ee0b4bbc0d262f2167
Benito Buchheim <github.benito.2017@buchheims.de>
2022-12-03 12:52:16 +0100

    using tile endpoint in frontend

--- backend/app/app/api/tiles_v1/endpoints/tiles.py
+++ backend/app/app/api/tiles_v1/endpoints/tiles.py
@@ -15,25 +15,24 @@ router = APIRouter()
     responses={200: {"content": {"image/png": {}}}},
 )
 def get_tile(
-    zoom: int,
-    xcoord: int,
-    ycoord: int,
+    zoom: int, xcoord: int, ycoord: int,
 ):
     """
     Get tile from cache or render
     """
 
     cached_tile_path = Path("/tiles") / str(zoom) / str(xcoord) / f"{ycoord}.png"
-    try:
-        # try loading file from cache
+    if cached_tile_path.exists():
         with open(cached_tile_path, "rb") as f:
             tile_image = f.read()
-    except FileNotFoundError:
-        # when no cached file found, recreate tile and store to cache
+    else:
         render_task = celery_app.send_task(
             "app.worker.render_tile", args=[zoom, xcoord, ycoord]
         )
-        tile_image = base64.b64decode(render_task.get(timeout=5))
+        # TODO: sometimes the task ran into a timeout
+        # although execution is successful and fast
+        # could not reproduce this error
+        tile_image = base64.b64decode(render_task.get(timeout=10))
         cached_tile_path.parent.mkdir(parents=True, exist_ok=True)
         with open(cached_tile_path, "wb") as f:
             f.write(tile_image)
--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -12,6 +12,18 @@ if (env === "production") {
   envApiUrl = `http://${process.env.VUE_APP_DOMAIN_LOCAL}`;
 }
 
+let envTilesUrl = "";
+if (env === "production") {
+  envTilesUrl = `https://${process.env.VUE_APP_TILES_DOMAIN_PROD}`;
+} else if (env === "staging") {
+  envTilesUrl = `https://${process.env.VUE_APP_TILES_DOMAIN_STAG}`;
+} else if (env === "development") {
+  envTilesUrl = `https://${process.env.VUE_APP_TILES_DOMAIN_DEV}`;
+} else {
+  envTilesUrl = `http://${process.env.VUE_APP_TILES_DOMAIN_LOCAL}`;
+}
+
 export const apiUrl = envApiUrl;
+export const tilesUrl = envTilesUrl;
 export const appName = process.env.VUE_APP_NAME;
 export const appVersion = process.env.VUE_APP_VERSION || "no version";
