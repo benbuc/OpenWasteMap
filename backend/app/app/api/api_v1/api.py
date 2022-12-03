48d29742501ba85376b45017b025bd78bbfb864b
Benito Buchheim <github.benito.2017@buchheims.de>
2022-12-03 12:56:51 +0100

    removed tiles endpoint from backend api

--- backend/app/app/api/api_v1/api.py
+++ backend/app/app/api/api_v1/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import admin, login, tiles, users, utils, waste_samples
+from app.api.api_v1.endpoints import admin, login, users, utils, waste_samples
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
@@ -9,5 +9,4 @@ api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(
     waste_samples.router, prefix="/waste-samples", tags=["waste_samples"]
 )
-api_router.include_router(tiles.router, prefix="/tiles", tags=["tiles"])
 api_router.include_router(admin.router, prefix="/admin", tags=["admin"])
